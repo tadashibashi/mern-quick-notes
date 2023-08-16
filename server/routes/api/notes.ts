96ffe13c728ba8023fa5279eb89722a81ce50dde
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-08-15 22:52:58 -0700

    simplify Note-create routing (we're not in Django-land anymore)

--- server/routes/api/notes.ts
+++ server/routes/api/notes.ts
@@ -4,7 +4,7 @@ const router = express.Router();
 import * as notesCtrl from "../../controllers/api/notes";
 import requireLogin from "../../middleware/requireLogin";
 
-router.post("/create", requireLogin(), notesCtrl.createOne);
+router.post("/", requireLogin(), notesCtrl.createOne);
 router.put("/:id", requireLogin(), notesCtrl.updateOne);
 router.delete("/:id", requireLogin(), notesCtrl.deleteOne);
 router.get("/", requireLogin(), notesCtrl.readUserOwned);
